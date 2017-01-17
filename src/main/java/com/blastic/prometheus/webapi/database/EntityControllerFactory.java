f143009235cbf11d39d73d9a39ebc29287acdf23
Cristobal Romero Rossi <cristobalromerorossi@gmail.com>
2017-01-16 19:06:08 -0500

    Adición: arquitectura del proyecto.

---
+++ src/main/java/com/blastic/prometheus/webapi/database/EntityControllerFactory.java
@@ -0,0 +1,14 @@
+package com.blastic.prometheus.webapi.database;
+
+/**
+ * The <strong>EntityControllerFactory</strong> class provided methods to get
+ * entities controllers.
+ *
+ * @author Gustavo Pacheco <ryctabo@gmail.com>
+ * @author Cristobal Romero Rossi <cristobalromerorossi@gmail.com>
+ *
+ * @version 1.0-SNAPSHOT
+ */
+public class EntityControllerFactory {
+
+}
---
+++ src/main/java/com/blastic/prometheus/webapi/model/OrderType.java
@@ -0,0 +1,10 @@
+package com.blastic.prometheus.webapi.model;
+
+/**
+ *
+ * @author Luis Alfonso Lenes Salas <luislenes02@gmail.com>
+ * @version 1.0
+ */
+public enum OrderType {
+    ASC, DESC
+}
